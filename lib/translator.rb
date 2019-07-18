ead706153de262edbc971b786c82a3a34c0136e5
Matt Werner <mrwerner392@gmail.com>
2019-07-18 02:56:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,12 @@ def get_japanese_emoticon(filepath, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(filepath, emoticon)
+  emoticons = load_library(filepath)
+  emoticons["get_meaning"].each do |key, value|
+    if emoticon == key
+      return value
+    end
+  end
+  return "Sorry, that emoticon was not found"
 end
